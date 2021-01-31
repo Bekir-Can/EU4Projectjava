c3a8b0d33057afaa3ee8e79e38e61c68b977331f
Bekir-Can <ruikosta03@gmail.com>
2021-01-31 22:22:59 +0200

    project is already in remote repo , comiting more changes

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         System.out.println("Hello");
         //EU4 is good at IntelliJ
 
+        //I have already published on Github.com
+        //now I am ready to make make commits
+        //but do I need to share again????
+        //ABSOLUTELY NOOOOOO
+        //It's time commit and push!!!!!!
+
     }
 }
